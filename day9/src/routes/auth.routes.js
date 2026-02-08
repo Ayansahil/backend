a69be19cee4d844868fbb95539470740a16e58a4
Ayan Sahil <ayans8557@gmail.com>
2026-02-08 14:04:47 +0530

    add hash in password with md5

--- day9/src/routes/auth.routes.js
+++ day9/src/routes/auth.routes.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const userModel = require("../models/user.model");
 const jwt = require("jsonwebtoken");
-
 const authRouter = express.Router();
+const crypto = require("crypto");
+
+/**
+ * /api/auth/register
+ */
 
 authRouter.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
@@ -14,10 +18,12 @@ authRouter.post("/register", async (req, res) => {
     });
   }
 
+  const hash = crypto.createHash("md5").update(password).digest("hex");
+
   const user = await userModel.create({
     name,
     email,
-    password,
+    password: hash,
   });
 
   const token = jwt.sign(
@@ -34,6 +40,51 @@ authRouter.post("/register", async (req, res) => {
     message: "user created successfully✅",
     user,
     token,
+  });
+});
+
+// authRouter.post('/protected',async(req,res)=>{
+//   console.log(req.cookies);
+
+//   res.status(200).json({
+//     message:"This is protected route"
+//   })
+// })
+
+/**
+ * POST /api/auth/login
+ */
+
+authRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await userModel.findOne({ email });
+
+  if (!user) {
+    return res.status(401).json({
+      message: "user not found with this email address",
+    });
+  }
+
+  const isPasswordMatched = user.password === crypto.createHash("md5").update(password).digest("hex");
+
+  if (!isPasswordMatched) {
+    return res.status(401).json({
+      message: "Invalid password",
+    });
+  }
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+      email: user.email,
+    },
+    process.env.JWT_SECRET,
+  );
+  res.cookie("jwt_token", token);
+
+  res.status(200).json({
+    message: "user logged in",
+    user,
   });
 });
 
